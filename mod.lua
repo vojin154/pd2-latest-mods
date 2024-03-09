9b6e25cd67e9c554d1a6e3d27b12be0b73564e4f
vojin154 <38527585+vojin154@users.noreply.github.com>
2024-03-09 12:55:23 +0100

    Some minor changes.

--- mod.lua
+++ mod.lua
@@ -12,36 +12,39 @@ if not HttpRequest then
     return
 end
 
-local notifications = BLT.Notifications --Got a bit annoying having to type BLT., so I did this.
+local notifications = BLT.Notifications --Got a bit annoying having to type BLT., so I did this. There might be some performance saving, but it doesn't really matter on such a simple mod
 local save_path = SavePath .. "LatestMods.txt"
 local url = "https://api.modworkshop.net/games/payday-2/mods"
 local key = Idstring("LatestMods"):key()
 
 local mods = {}
 local index = 1
-local limit = 5 --by https://api.modworkshop.net
+local limit = 5
 --[[
-limit (integer)
-Must be at least 1. Must not be greater than 50.
+    by https://api.modworkshop.net
+    limit (integer)
+    Must be at least 1. Must not be greater than 50.
 ]]
 local id
 local swap = 10
 
 local function makeNotif()
-    if id then
+    if notifications:_get_notification(id) then
         notifications:remove_notification(id)
         id = nil
     end
 
-    local text = string.format("%s \nBy %s", mods[index].name, mods[index].user.name)
+    local text = string.format("%s \nBy %s", mods[index].name, mods[index].user_name)
     local params = {
         title = string.format("%i LATEST MODS", limit),
         text = text,
         callback = function(notif_id) --On clicked event.
             managers.network.account:overlay_activate("url", "https://modworkshop.net/mod/" .. mods[index].id)
 
+            --Here we follow rule #2 -"Do not replicate the site or remove the need to visit the site to download mods.", so we just redirect to the website with the mod id.
+
             --[[
-                Apparently epic also has overlay? At least that's what I'm understanding here.
+                Apparently Epic also has overlay? At least that's what I'm understanding here.
 
                 function NetworkAccountEPIC:overlay_activate(...)
                 	if self._overlay_opened then
@@ -67,7 +70,11 @@ end
 local function makeModsList(data)
     for i, v in ipairs(data) do
         if not (i > limit) then
-            mods[i] = v
+            mods[i] = { --No need to save all the mod info, if we won't use it.
+                id = v.id,
+                name = v.name,
+                user_name = v.user.name
+            }
         else
             break --No need to keep parsing, if we got what we need.
         end
@@ -117,17 +124,12 @@ local clbk = function(result, body) --Returns as result false, if it failed, wit
 end
 
 --[[
+    Lmao I wrote this before I even got an idea how to do this.
+
+
     NOTE:
     I have no fucking idea how do headers work so have fun looking at this.. as it is really hideous.
 ]]
-
-local headers = {
-    ["Accept"] = "application/json"
-}
-
-local body = { --Doesn't seem to work so imma stick with a workaround meanwhile
-    ["limit"] = 5
-}
 
 local update = true --To avoid some mass spamming and since mods dont get released/updated that often, there's no point in constantly checking.
 local file = io.open(save_path, "r")
@@ -138,12 +140,20 @@ if file then
     local delay = 60 * 5 --I'd say 5 minutes delay is good enough.
     if value then
         local time_since = os.time() - value["time"]
-        if time_since < delay then
+        if time_since < delay then --This is to ensure we don't spam the ModWorkshops API, and also to follow rule #1 - "Do not spam the API.".
             update = false
             mods = value["mods"]
         end
     end
 end
+
+local headers = {
+    ["Accept"] = "application/json"
+}
+
+local body = { --Doesn't seem to work so imma stick with a workaround meanwhile.
+    ["limit"] = 5
+}
 
 if update then
     HttpRequest:get(url, clbk, headers, key)
@@ -162,9 +172,9 @@ HttpGet()
 
 local time = 0
 Hooks:PostHook(MenuManager, "update", "update_latest_mods", function(self, t, dt, ...)
-    time = time + dt
-    if time > swap then
-        next()
-        time = 0
+    time = time + dt --Increase the time.
+    if time > swap then --If time is greater than the amount of seconds set in swap then.
+        next() --Increase index and make a new notif with that info.
+        time = 0 --Reset the time.
     end
 end)
